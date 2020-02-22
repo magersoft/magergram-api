c165929d87b1216e687f509fdfe228a1427b2f86
v.mager <v.mager@b2b-center.ru>
2020-02-22 22:23:45 +0300

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+import { GraphQLServer } from 'graphql-yoga';
+import logger from 'morgan';
+import schema from './schema';
+
+const PORT = process.env.PORT || 4000;
+
+const server = new GraphQLServer({ schema });
+
+server.express.use(logger('dev'));
+
+server.start({ port: PORT }, () => console.log(`Server running on port http://localhost:${PORT}`));
