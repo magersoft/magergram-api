b76d2f18ba04df23fc02fff921d0026391db2862
v.mager <v.mager@b2b-center.ru>
2020-02-23 02:00:10 +0300

    sendEmail

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
-require('dotenv').config();
-
+import dotenv from 'dotenv';
+import path from 'path';
 import { GraphQLServer } from 'graphql-yoga';
 import logger from 'morgan';
 import schema from './schema';
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 const PORT = process.env.PORT || 4000;
 
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,33 @@
+import dotenv from 'dotenv';
+import path from 'path';
 import { adjectives, nouns } from './words';
+import nodemailer from 'nodemailer';
+import sgTransport from 'nodemailer-sendgrid-transport';
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`
+};
+
+export const sendMail = email => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD
+    }
+  };
+  const client = nodemailer.createTransport(sgTransport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: 'noreply@magergram.com',
+    to: address,
+    subject: '🔐 Login Secret for Magergram',
+    html: `Hello! Your login secret is ${secret}.<br/> Copy paste on the app/website to login`
+  };
+  return sendMail(email);
 };
