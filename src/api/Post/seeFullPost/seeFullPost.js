f9765047253b9c04c47d9d96faa1b8b889208ca0
v.mager <v.mager@b2b-center.ru>
2020-02-28 00:09:49 +0300

    Refactor api

--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -2,15 +2,6 @@ import { prisma } from '../../../../generated/prisma-client';
 
 export default {
   Query: {
-    seeFullPost: async (_, args) => {
-      const {id} = args;
-      const post = await prisma.post({id});
-      const comments = await prisma.post({id}).comments;
-      const files = await prisma.post({ id }).files;
-      const user = await prisma.post({ id }).user();
-      return {
-        post, comments, files, user
-      }
-    }
+    seeFullPost: (_, args) => prisma.post(args)
   }
 };
