3320a8a8ad8db9b3b95487a8c292f5f2abcdcd7e
v.mager <v.mager@b2b-center.ru>
2020-04-01 01:08:25 +0300

    firebase config services

--- src/firebase.js
+++ src/firebase.js
@@ -4,28 +4,24 @@ const uuid = require('uuid-v4');
 const privateKey = require('./firebase_key');
 
 const {
-  FIREBASE_TYPE,
   FIREBASE_PROJECT_ID,
   FIREBASE_KEY_ID,
   FIREBASE_CLIENT_EMAIL,
   FIREBASE_CLIENT_ID,
-  FIREBASE_AUTH_URI,
-  FIREBASE_TOKEN_URI,
-  FIREBASE_AUTH_PROVIDER_URL,
   FIREBASE_CLIENT_AUTH_URL
 } = process.env;
 
 const serviceAccount = {
-  "type": FIREBASE_TYPE,
-  "project_id": FIREBASE_PROJECT_ID,
-  "private_key_id": FIREBASE_KEY_ID,
-  "private_key": privateKey['private_key'],
-  "client_email": FIREBASE_CLIENT_EMAIL,
-  "client_id": FIREBASE_CLIENT_ID,
-  "auth_uri": FIREBASE_AUTH_URI,
-  "token_uri": FIREBASE_TOKEN_URI,
-  "auth_provider_x509_cert_url": FIREBASE_AUTH_PROVIDER_URL,
-  "client_x509_cert_url": FIREBASE_CLIENT_AUTH_URL
+  'type': 'service_account',
+  'project_id': FIREBASE_PROJECT_ID,
+  'private_key_id': FIREBASE_KEY_ID,
+  'private_key': privateKey['private_key'],
+  'client_email': FIREBASE_CLIENT_EMAIL,
+  'client_id': FIREBASE_CLIENT_ID,
+  'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
+  'token_uri': 'https://oauth2.googleapis.com/token',
+  'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
+  'client_x509_cert_url': FIREBASE_CLIENT_AUTH_URL
 };
 
 firebase.initializeApp({
