8f32b34c846fda273c89313affd4a9a89ffa68a5
v.mager <v.mager@b2b-center.ru>
2020-04-23 18:12:44 +0300

    fix: versio

--- src/api/App/getVersion/getVersion.ts
+++ src/api/App/getVersion/getVersion.ts
@@ -3,6 +3,7 @@ const pkg = require('../../../../package.json');
 export default {
   Query: {
     getVersion: (): string => {
+      console.log('Version', pkg.version);
       return pkg.version;
     }
   }
