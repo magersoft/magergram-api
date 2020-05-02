feeb0b5d5572e53f4c3c100ceee5f838ce4ef136
v.mager <v.mager@b2b-center.ru>
2020-05-02 18:03:53 +0300

    featureL validate phone number in edit User

--- src/api/User/editUser/editUser.resolvers.ts
+++ src/api/User/editUser/editUser.resolvers.ts
@@ -1,17 +1,40 @@
 import { isAuthenticated } from '../../../middlewares';
 import { prisma, User } from '../../../generated/prisma-client';
+import { isValidPhone } from '../../../utils/sendSMS';
 
 export default {
   Mutation: {
-    editUser: async (_, args, { request }): Promise<User> => {
+    editUser: async (_, args, { request }): Promise<any> => {
       isAuthenticated(request);
       const user: User = request.user;
-      return await prisma.updateUser({
-        where: {
-          id: user.id
-        },
-        data: args
-      })
+
+      try {
+        const { phone } = args;
+        let preparedPhone;
+        if (phone) {
+          if (!phone.match(isValidPhone)) {
+            throw new Error('Enter a valid phone number');
+          }
+          preparedPhone = phone.replace('+', '').replace('8', '7');
+        }
+        const updatedUser = await prisma.updateUser({
+          where: {
+            id: user.id
+          },
+          data: { ...args, phone: preparedPhone }
+        });
+        return {
+          ok: true,
+          data: updatedUser,
+          error: null
+        }
+      } catch (e) {
+        return {
+          ok: false,
+          data: null,
+          error: e.message
+        }
+      }
     }
   }
 }
